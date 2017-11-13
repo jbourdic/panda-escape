35d3836f28a8e010c29b1cb796a1ec1b160d5c8c
julien <julien.bourdic@gmail.com>
2017-11-13 19:05:58 +0100

    reducers

--- src/reducers/controls.js
+++ src/reducers/controls.js
@@ -6,9 +6,20 @@ import {
 } from '../actions/controls'
 
 //todo : https://redux.js.org/docs/basics/Actions.html
+const initialState = {
+    players: [],
+    grid: []
+};
 
+function pandaApp(state = initialState, action) {
+    switch (action.type) {
+        case MOVE:
+            return Object.assign({}, state, action.direction)
+    }
+
+    return state;
+}
 const controlsApp = combineReducers({
-
 });
 
 export default controlsApp;
